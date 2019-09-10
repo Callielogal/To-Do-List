294958f06733d66d3d42e23fd55937a263a9cd6a
Bernier Elodie <kalikantzaros21@hotmail.com>
2019-09-10 15:53:28 +0200

    try cli

---
+++ kanban/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <h1>
+            Webpack sla merde
+        </h1>
+        <alert></alert>
+    </div>
+</template>
+
+
+<script>
+import Alert from './components/alert.vue'
+export default {
+    name: "app",
+    components:{
+        Alert
+        }
+}
+</script>
+
+<style lang="scss">
+$color : red;
+h1{
+    background-color: $color;
+    width: 30vw;
+    margin: auto;
+    text-align: center;
+}
+</style>
---
+++ kanban/src/components/alert.vue
@@ -0,0 +1,56 @@
+<template>
+<div  @click="change" :class="classname">
+    {{help}} {{message}}
+</div> 
+</template>
+
+
+    <script>
+    export default {
+        name: "Alert",
+        props: ['message'],
+        data: function()
+        {
+            return{
+                help: "besoin d'aide?",
+                classname: "alert alert-primary"
+               }
+        },
+        methods:{
+            change: function()
+            {
+                if (this.classname === "alert alert-primary") {
+                    this.classname = "alert alert-success"
+                }
+                else
+                this.classname = "alert alert-primary"
+            }
+
+        }
+
+    } 
+    </script>
+
+    <style lang="scss">
+    div{
+        height:10vh;
+    }
+    .alert{
+        position: relative;
+        padding:.75 rem 1.25 rem;
+        margin-bottom: 1 rem;
+        border: 1 px solid transparent;
+        border-radius: .25 rem;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    .alert-primary{
+        color: #004085;
+        background-color: #cce5ff;
+    }
+    .alert-success{
+        color: white;
+        background-color: chocolate;
+    }
+    </style>
